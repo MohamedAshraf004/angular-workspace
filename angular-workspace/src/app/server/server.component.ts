2f53bf77b716c2aa3d9a5a4f1fac69a96e2a21f4
Mohamed Ashraf <lionelmido004@gmail.com>
2023-09-03 06:31:20 +0000

    event binding & property binding & two way binding

--- angular-workspace/src/app/server/server.component.ts
+++ angular-workspace/src/app/server/server.component.ts
@@ -6,21 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-allowedAddServer = false;
-serverId = 5;
-status : string = 'Active';
-creatioStatus = "No Creation";
-constructor(){
-  setTimeout(()=>{
-    this.allowedAddServer = true
-  },2000)
-}
+    allowedAddServer = false;
+    serverId = 5;
+    status : string = 'Active';
+    creatioStatus = "No Creation";
+    serverName: string = ""
+    constructor(){
+      setTimeout(()=>{
+        this.allowedAddServer = true
+      },2000)
+    }
 
-getServerStatus() : string{
-return this.status;
-}
+    getServerStatus() : string{
+    return this.status;
+    }
 
-onServerCreationStatus(){
-  this.creatioStatus = "Created";  
-}
+    onServerCreationStatus(){
+      this.creatioStatus = "Server Is Created! Server Name Is : " + this.serverName;  
+    }
+    onServerUpdateName(event:Event){
+      let target =(<HTMLInputElement>event.target);
+    this.serverName = target.value;
+      console.log(target.value)
+    }
+
 }
